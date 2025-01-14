649e9163f1f8e2883d7fb03058a2acfd9721ca6f
lucyann <lucyann78@gmail.com>
2025-01-14 15:10:59 -0400

    corrected factorial calculation

---
+++ debugging/factorial.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  # Decrement n to avoid infinite loop
+    return result
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./factorial.py <number>")
+        sys.exit(1)
+
+    f = factorial(int(sys.argv[1]))
+    print(f)
